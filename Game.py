3781d0cca2ed66f1d0da56e9bdc67433be871acb
Serguei Balanovich <sbalanovich@college.harvard.edu>
2015-04-18 20:14:23 -0400

    Added game skeleton

---
+++ Game.py
@@ -0,0 +1,19 @@
+class Player(object):
+
+    def __init__(self):
+        self.something = something
+
+class Piece(object):
+
+    def __init__(self):
+        self.something = something
+
+class Board(object):
+
+    def __init__(self):
+        self.something = something
+
+class Game(object):
+
+    def __init__(self):
+        self.something = something
